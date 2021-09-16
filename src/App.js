0e71506847cf18b5094f46673555212cdeb3b74e
huseyincakmak <xhuseyincakmak@gmail.com>
2021-09-16 17:08:55 +0300

    Add BMI page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import BMI from './pages/BMI';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+    <BMI/>
+    </>
   );
 }
 
---
+++ src/pages/BMI.js
@@ -0,0 +1,13 @@
+
+
+export default function BMI() {
+    return (
+        <>
+        <h1>What is the body mass index (BMI)?</h1>
+         <p>
+         The body mass index (BMI) is a measure that uses your height and weight to work out if your weight is healthy.
+         The BMI calculation divides an adult's weight in kilograms by their height in metres squared. For example, A BMI of 25 means 25kg/m2.
+         </p>
+        </>
+    )
+}
